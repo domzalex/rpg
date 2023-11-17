e62b4d85874cf472d27f63cca345afbd9db0fd29
Alex Domzalski <alexdomzalski@gmail.com>
2023-11-16 20:51:35 -0500

    removed redundant/unnecessary files

--- src/tilesets/fores-tiletset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="late2023" tilewidth="16" tileheight="16" tilecount="4625" columns="125">
- <image source="../img/forest_tileset.png" width="2000" height="592"/>
-</tileset>
